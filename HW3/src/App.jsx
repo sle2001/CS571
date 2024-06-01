b61f539b9afbda3bc478ef5e711d01f575a43906
Steven Le <136869060+sle2001@users.noreply.github.com>
2024-05-31 22:22:02 -0400

    Delete HW3 directory

--- HW3/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import BadgerMart from './components/BadgerMart'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return <BadgerMart/>
-}
-
-export default App
